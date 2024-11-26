7d99d7733d282b008b4aa8a7a20b334d62782b76
Ahalya <ahalyagirish02@gmail.com>
2024-11-26 10:36:04 +0530

    created receptionist repository

---
+++ Attk_Final/Controllers/ReceptionistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Attk_Final.Controllers
+{
+    public class ReceptionistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
